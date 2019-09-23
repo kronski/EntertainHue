31f5d3046e95926bfc61f89eb9ec2b18057ad253
anders.kronlund@gmail.com <anders.kronlund@gmail.com>
2019-09-23 22:54:18 +0200

    find hue bridge

--- src/entertainhue.py
+++ src/entertainhue.py
@@ -2,6 +2,9 @@
 
 import socket
 
+from http_parser.parser import HttpParser
+
+
 msg = \
     'M-SEARCH * HTTP/1.1\r\n' \
     'HOST:239.255.255.250:1900\r\n' \
@@ -12,12 +15,25 @@ msg = \
 
 # Set up UDP socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-s.settimeout(2)
+s.settimeout(5)
 s.sendto(msg.encode('utf-8'), ('239.255.255.250', 1900) )
 
 try:
     while True:
         data, addr = s.recvfrom(65507)
-        print(addr, data)
+        p = HttpParser()
+        recved = len(data)
+        nparsed = p.execute(data, recved)
+        assert nparsed == recved
+        if p.is_headers_complete():
+            headers = p.get_headers()
+            if 'hue-bridgeid' in headers:
+                print(addr)
+                for key in headers:
+                    print("\t",key,":",headers[key])
+                break
+
+        if p.is_message_complete():
+            break
 except socket.timeout:
     pass
