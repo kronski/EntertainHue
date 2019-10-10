ee555908da17420928fef692c9366c17f94a6f26
anders.kronlund@gmail.com <anders.kronlund@gmail.com>
2019-10-10 21:44:34 +0200

    pi camera added

--- src/entertainhue.py
+++ src/entertainhue.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
 
 import sys
-from http_parser.parser import HttpParser
+try:
+    from http_parser.parser import HttpParser  # pylint: disable=no-name-in-module
+except ImportError:
+    from http_parser.pyparser import HttpParser
 import argparse
 import requests 
 import time
 import json
 from pathlib import Path
-import ssl
 from socket import socket, AF_INET, SOCK_DGRAM, IPPROTO_UDP, timeout
-from dtls import do_patch
-import sslpsk
-import binascii
 import subprocess
 import threading
 import random
+import fileinput
+import picamera
+import picamera.array
+import numpy as np
+import errno
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-v","--verbose", dest="verbose", action="store_true")
+parser.add_argument("-s","--send", dest="sendtolight", action="store_true")
+parser.add_argument("-r","--random", dest="random", action="store_true")
+parser.add_argument("-c","--picamera", dest="picamera", action="store_true")
 parser.add_argument("-g","--groupid", dest="groupid")
 commandlineargs = parser.parse_args()
 
@@ -149,7 +156,6 @@ verbose("Using groupid={}".format(groupid))
 
 r = requests.get(url = baseurl+"{}/groups/{}".format(clientdata['username'],groupid))
 jsondata = r.json()
-print(jsondata)
 
 
 verbose("Enabling streaming on group")
@@ -157,61 +163,113 @@ r = requests.put(url = baseurl+"{}/groups/{}".format(clientdata['username'],grou
 jsondata = r.json()
 verbose(jsondata)
 
-def output_reader(proc):
-    #for line in iter(proc.stdout.readline, b''):
-    #    if not line:
-    #        break
-    #    print('got line: {0}'.format(line), end='')
-    print('x')
+bufferlock = threading.Lock()
+stopped = False
+rgb1 = bytearray([255,255,255,255,255,255])
+rgb2 = bytearray([255,255,255,255,255,255])
+camera = None
 
-def output_writer(proc):
-    while True:
+def stdin_to_buffer():
+    for line in fileinput.input():
+        print(line)
+        if stopped:
+            break
 
+def random_to_buffer():
+    global rgb1
+    global rgb2
+    while not stopped:
+        bufferlock.acquire()
         rgb1 = bytearray([random.randint(0, 255),random.randint(0, 255),random.randint(0, 255),random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)])
         rgb2 = bytearray([random.randint(0, 255),random.randint(0, 255),random.randint(0, 255),random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)])
-
+        bufferlock.release()
+        time.sleep(.1)
+        
+def buffer_to_light(proc):
+    while not stopped:
         # message = bytes('HueStream','utf-8')+b'\1\0\0\0\0\0\0'+b'\0\0\x0B\xFF\xFF\x00\x00\x00\x00'+b'\0\0\x0C\xFF\xFF\x00\x00\xFF\xFF'
+        bufferlock.acquire()
         message = bytes('HueStream','utf-8')+b'\1\0\0\0\0\0\0'+b'\0\0\x0B'+rgb1+b'\0\0\x0C'+rgb2
+        bufferlock.release()
         #                                      V V S R R C R 
-        print(len(message))
         proc.stdin.write(message)
         proc.stdin.flush()
-        print("sent")
         time.sleep(.2)
 
+class MyAnalysis(picamera.array.PiRGBAnalysis):
+    def __init__(self, camera):
+        super(MyAnalysis, self).__init__(camera)
+        self.frame_num = 0
+        self.halfwidth = camera.resolution[0]>>1
+
+    def analyse(self, a):
+        global rgb1
+        global rgb2
+        global bufferlock
+        lr = int(np.mean(a[:self.halfwidth,:, 0]))
+        lg = int(np.mean(a[:self.halfwidth,:, 1]))
+        lb = int(np.mean(a[:self.halfwidth,:, 2]))
+
+        rr = int(np.mean(a[self.halfwidth:,:, 0]))
+        rg = int(np.mean(a[self.halfwidth:,:, 1]))
+        rb = int(np.mean(a[self.halfwidth:,:, 2]))
+
+        bufferlock.acquire()
+        rgb1 = bytearray([lr,0,lg,0,lb,0])
+        rgb2 = bytearray([rr,0,rg,0,rb,0])
+        bufferlock.release()
+        self.frame_num += 1
+
+def picamera_to_buffer():
+    global camera
+    with picamera.PiCamera() as camera:
+        camera.resolution = (320, 240)
+        camera.framerate = 30
+
+        camera.zoom = (0.36, 0.52, 0.19, 0.19)
+        camera.vflip = True
+        camera.hflip = True
+        with MyAnalysis(camera) as output:
+            camera.start_recording(output, 'rgb')
+            camera.wait_recording(86400)
+            
+
 try:
-    #do_patch()
-
-    #verbose('Unhexing clientkey')
-    #clientkey = binascii.unhexlify(clientdata['clientkey'])
-    #verbose(clientdata['clientkey'],"=>", clientkey)
-
-    # sock = sslpsk.wrap_socket(socket(AF_INET, SOCK_DGRAM),psk=(clientdata['username'],clientkey))
-    # sock.connect((hueip, 2100))
-    # sock.close()
-    # result = subprocess.run(['ls', '-l'], stdout=subprocess.PIPE)
-    # print(result.stdout)
-
-    #for path in execute(["openssl","s_client","-dtls1_2","-cipher","DHE-PSK-AES256-GCM-SHA384","-psk_identity",clientdata['username'],"-psk",clientdata['clientkey'], "-connect", hueip+":2100"]):
-    #    print(path, end="")
     try:
-        cmd = ["openssl","s_client","-dtls1_2","-cipher","DHE-PSK-AES256-GCM-SHA384","-psk_identity",clientdata['username'],"-psk",clientdata['clientkey'], "-connect", hueip+":2100"]
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=False)
+        threads = list()
 
-        print(proc.stdin)
+        if commandlineargs.random:
+            verbose("Starting randomizer")
+            t = threading.Thread(target=random_to_buffer)
+            t.start()
+            threads.append(t)
+        elif commandlineargs.picamera:
+            verbose("Starting picamera")
+            t = threading.Thread(target=picamera_to_buffer)
+            t.start()
+            threads.append(t)
 
-        reader = threading.Thread(target=output_reader, args=(proc,))
-        writer = threading.Thread(target=output_writer, args=(proc,))
-        reader.start()
-        writer.start()
+        if commandlineargs.sendtolight:
+            verbose("Starting send to light")
+            cmd = ["openssl","s_client","-dtls1_2","-cipher","DHE-PSK-AES256-GCM-SHA384","-psk_identity",clientdata['username'],"-psk",clientdata['clientkey'], "-connect", hueip+":2100"]
+            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=False)
+            t = threading.Thread(target=buffer_to_light, args=(proc,))
+            t.start()
+            threads.append(t)
+
+
 
         input("Press return to stop")
-        proc.stdin.close()
-
-        reader.join()
-        writer.join()
+        stopped=True
+        if isinstance(camera,picamera.PiCamera):
+            if camera.recording:
+                camera.stop_recording()
+        
+        for t in threads:
+            t.join()
     except Exception as e:
         print(e)
+        stopped=True
 
     # input("Press Enter to continue...")
 finally:
